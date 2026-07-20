21cc35e8bb28938cb6181f992ba0f1c37280d06d
Andy9b99 <andrewbia818@gmail.com>
2026-07-20 10:09:55 -0400

    D:Added the commands map with exit and help commands.

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Create a new scanner to read from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+	// Start an infinite for loop that  will execute once for
+	//  every command the user types in
+	for {
+		// Use fmt.Print to print the prompt "Pokedex >"
+		//  without a newline character
+		fmt.Print("Pokedex > ")
+		// Read a line from standard input
+		if !scanner.Scan() {
+			break
+		}
+		// Get the text from the scanner
+		text := scanner.Text()
+		// Call the cleanInput function with the text
+		words := cleanInput(text)
+		//Update the REPL loop to use the "command" (first word)
+		if len(words) == 0 {
+			continue
+		}
+		command, exists := commands[words[0]]
+		if !exists {
+			fmt.Println("Unknown command")
+			continue
+		}
+		err := command.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 }
--- repl.go
+++ repl.go
@@ -1,17 +1,57 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"strings"
 )
 
-// need to implement the cleanInput function that takes
-// a string input and returns a slice of strings
-// The function should clean the input by removing leading
-// and trailing whitespace, converting the string to lowercase,
-// and splitting it into words based on whitespace between them.
+// cleanInput function takes a string input, returns a slice of strings
+// removes leading & trailing whitespace, does toLower()
+// & splits into words based on whitespace between them.
+var words []string
+
 func cleanInput(text string) []string {
 	text = strings.TrimSpace(text)
 	text = strings.ToLower(text)
-	words := strings.Fields(text)
+	words = strings.Fields(text)
 	return words
+}
+
+// Add an exit command function.
+func commandExit() error {
+	if len(words) > 0 && words[0] == "exit" {
+		fmt.Println("Closing the Pokedex... Goodbye!")
+		os.Exit(0)
+	}
+	return nil
+}
+
+// Add a help command function.
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!\nUsage:\n")
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+	return nil
+}
+
+// Define a struct to represent all commands in the CLI.
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+// Create a map to hold all the commands in the CLI.
+var commands = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+	"help": {
+		name:        "help",
+		description: "Display a list of all commands",
+		callback:    commandHelp,
+	},
 }
